9943a963f9b6e4eece8aa2e57d75799c164dc653
Matvey <smirnv.matvejj@yandex.ru>
2023-02-18 16:30:37 +0300

    added dynamic imports/lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,15 @@
+import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import {MainPage, ComicsPage, Page404, SinglePage} from '../pages';
 import SingleCharacterLayout from '../pages/singleCharacterLayout/SingleCharacterLayout';
 import SingleComicLayout from '../pages/singleComicLayout/SingleComicLayout';
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import ('../pages/404'));
+const MainPage = lazy(() => import ('../pages/MainPage'));
+const ComicsPage = lazy(() => import ('../pages/ComicsPage'));
+const SinglePage = lazy(() => import ('../pages/SinglePage'));
 
 const App = () => {
     
@@ -12,13 +18,15 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>
-                        <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
